71be96ed70b2dad911882cb05df40b7e80d8823f
SNIPELEON <142342432+SNIPELEON@users.noreply.github.com>
2024-09-20 10:22:29 +0530

    Update ool.py

--- python/ool.py
+++ python/ool.py
@@ -1,6 +1,13 @@
 import streamlit as st
-name=st.text_input("enter the text:")
-if st.button("reverse"):
-    st.write(name[::-1])
-if st.button("print first 4 letters"):
-    st.write(name[0:5])
+class buttons:
+    def __init__(self,num):
+        if st.button(f"button number is {num}"):
+            st.write(num*num)
+            self.calc(num)
+    def calc(self,num):
+        if(num %2 ==0):
+            st.balloons()
+        else:
+            st.snow()
+for i in range(0,10,3):
+    buttons(i)
